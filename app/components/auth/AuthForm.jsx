e01ab9fe80292e18603333370622c7a3e566c63e
claudia_meneghesso <claudia.meneghesso@yld.io>
2022-12-20 12:37:13 +0000

    feature/ Added Url Search Params to login/logout

--- app/components/auth/AuthForm.jsx
+++ app/components/auth/AuthForm.jsx
@@ -1,11 +1,23 @@
-import { FaLock } from 'react-icons/fa';
+import { FaLock, FaUserPlus } from "react-icons/fa";
+
+import { Link, useSearchParams } from "@remix-run/react";
 
 function AuthForm() {
+  const [searchParams] = useSearchParams();
+
+  const authMode = searchParams.get("mode") || "login";
+
+  const isLogIn = authMode === "login";
+
+  const submitBtnCaption = isLogIn ? "Login" : "Register";
+
+  const toggleBtnCaption = isLogIn
+    ? "Create a new user"
+    : "Log in with existing user";
+
   return (
     <form method="post" className="form" id="auth-form">
-      <div className="icon-img">
-        <FaLock />
-      </div>
+      <div className="icon-img">{isLogIn ? <FaLock /> : <FaUserPlus />}</div>
       <p>
         <label htmlFor="email">Email Address</label>
         <input type="email" id="email" name="email" required />
@@ -15,8 +27,10 @@ function AuthForm() {
         <input type="password" id="password" name="password" minLength={7} />
       </p>
       <div className="form-actions">
-        <button>Login</button>
-        <a href="/auth">Log in with existing user</a>
+        <button>{submitBtnCaption}</button>
+        <Link to={isLogIn ? "?mode=signup" : "?mode=login"}>
+          {toggleBtnCaption}
+        </Link>
       </div>
     </form>
   );
