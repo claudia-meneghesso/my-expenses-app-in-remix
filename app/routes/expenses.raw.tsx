0c8206408edba082daecd6cc26aa3e66372d3799
claudia_meneghesso <claudia.meneghesso@yld.io>
2022-12-20 12:04:13 +0000

    feature/ Create a raw Resource Route

--- app/routes/expenses.raw.tsx
+++ app/routes/expenses.raw.tsx
@@ -1,3 +1,26 @@
 import { FC } from "react";
 
-export const loader = () => {};
+export const loader = () => {
+  const data = [
+    {
+      id: "e1",
+      title: "First expense",
+      amount: 16.22,
+      date: new Date().toISOString(),
+    },
+    {
+      id: "e2",
+      title: "Second expense",
+      amount: 45.0,
+      date: new Date().toISOString(),
+    },
+    {
+      id: "e3",
+      title: "Third expense",
+      amount: 84.34,
+      date: new Date().toISOString(),
+    },
+  ];
+
+  return data;
+};
--- app/routes/expenses/raw.tsx
+++
@@ -1,5 +0,0 @@
-import { FC } from "react";
-
-export const RawExpense: FC = () => <h1>RawExpense</h1>;
-
-export default RawExpense;
