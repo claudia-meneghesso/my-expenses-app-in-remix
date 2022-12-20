71762d386c931ca9665d9ae96284a2d9e42ecced
claudia_meneghesso <claudia.meneghesso@yld.io>
2022-12-20 12:47:08 +0000

    feature/ Add buttons to redirect to both add and raw routes in expenses layout

--- app/routes/__app/expenses.tsx
+++ app/routes/__app/expenses.tsx
@@ -1,4 +1,6 @@
-import { Outlet } from "@remix-run/react";
+import { Link, Outlet } from "@remix-run/react";
+
+import { FaPlus, FaDownload } from "react-icons/fa";
 
 import ExpensesList from "~/components/expenses/ExpensesList";
 import ExpensesHeader from "~/components/navigation/ExpensesHeader";
@@ -29,6 +31,17 @@ const ExpensesLayout = () => (
     <ExpensesHeader />
     <Outlet />
     <main>
+      <section id="expenses-actions">
+        <Link to="add">
+          <FaPlus />
+          <span>Add Expense</span>
+        </Link>
+
+        <a href="/expenses/raw">
+          <FaDownload />
+          <span>Load Raw Data</span>
+        </a>
+      </section>
       <ExpensesList expenses={data} />
     </main>
   </>
