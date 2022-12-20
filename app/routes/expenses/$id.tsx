9c46933a6378326e36c0b2a69607a5672d962da3
claudia_meneghesso <claudia.meneghesso@yld.io>
2022-12-20 11:26:52 +0000

    feature/ Swap anchor tags with remix Link and NavLink

--- app/routes/expenses/$id.tsx
+++ app/routes/expenses/$id.tsx
@@ -1,7 +1,21 @@
+import { useNavigate } from "@remix-run/react";
 import { FC } from "react";
 
 import ExpenseForm from "~/components/expenses/ExpenseForm";
+import Modal from "~/components/util/Modal";
 
-export const ExpenseDetailPage: FC = () => <ExpenseForm />;
+export const ExpenseDetailPage: FC = () => {
+  const navigate = useNavigate();
+
+  const handleCloseModal = () => {
+    navigate("/expenses");
+  };
+
+  return (
+    <Modal onClose={handleCloseModal}>
+      <ExpenseForm />
+    </Modal>
+  );
+};
 
 export default ExpenseDetailPage;
--- app/routes/expenses/add.tsx
+++ app/routes/expenses/add.tsx
@@ -1,13 +1,18 @@
+import { useNavigate } from "@remix-run/react";
 import { FC } from "react";
 
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
 
 export const ExpenseAddPage: FC = () => {
-  const handleModalClose = () => {};
+  const navigate = useNavigate();
+
+  const handleCloseModal = () => {
+    navigate("/expenses");
+  };
 
   return (
-    <Modal onClose={handleModalClose}>
+    <Modal onClose={handleCloseModal}>
       <ExpenseForm />
     </Modal>
   );
