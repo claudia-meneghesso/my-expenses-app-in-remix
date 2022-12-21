d9ec4c6e9e4447359d5ef9c701b464c683891062
claudia_meneghesso <claudia.meneghesso@yld.io>
2022-12-21 15:16:48 +0000

    feature/ Handle data loading in raw route

--- app/routes/expenses.raw.tsx
+++ app/routes/expenses.raw.tsx
@@ -1,26 +1,5 @@
-import { FC } from "react";
+import { getExpenses } from "~/data/expenses.server";
 
 export const loader = () => {
-  const data = [
-    {
-      id: "e1",
-      title: "First expense",
-      amount: 16.22,
-      date: new Date().toISOString(),
-    },
-    {
-      id: "e2",
-      title: "Second expense",
-      amount: 45.0,
-      date: new Date().toISOString(),
-    },
-    {
-      id: "e3",
-      title: "Third expense",
-      amount: 84.34,
-      date: new Date().toISOString(),
-    },
-  ];
-
-  return data;
+  return getExpenses();
 };
